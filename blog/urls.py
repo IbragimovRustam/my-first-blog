ec40d9f85c82bb49e315535f525d133968e72f01
Rustam <ibragimovr_d@mail.ru>
2019-02-11 11:49:12 +0600

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     # views.post_detail - это значит, что Django ожидает найти функцию-представление с названием post_detail в blog/views.py
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
